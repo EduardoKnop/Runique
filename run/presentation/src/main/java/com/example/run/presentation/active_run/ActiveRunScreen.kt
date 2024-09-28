b172339df4c8a2c707bd5fd25c89168205e8d7b2
Eduardo Knop <eduk.coelho@hotmail.com>
2024-09-28 09:34:50 -0300

    Creating Notification to Continue Running in the Background

--- run/presentation/src/main/java/com/example/run/presentation/active_run/ActiveRunScreen.kt
+++ run/presentation/src/main/java/com/example/run/presentation/active_run/ActiveRunScreen.kt
@@ -35,6 +35,7 @@ import com.example.core.presentation.designsystem.components.util.RuniqueToolbar
 import com.example.run.presentation.R
 import com.example.run.presentation.active_run.components.RunDataCard
 import com.example.run.presentation.active_run.maps.TrackerMap
+import com.example.run.presentation.active_run.service.ActiveRunService
 import com.example.run.presentation.util.hasLocationPermission
 import com.example.run.presentation.util.hasNotificationPermission
 import com.example.run.presentation.util.shouldShowLocationPermissionRationale
@@ -43,11 +44,13 @@ import org.koin.androidx.compose.koinViewModel
 
 @Composable
 fun ActiveRunScreenRoot(
+    onServiceToggle: (isServiceRunning: Boolean) -> Unit,
     modifier: Modifier = Modifier,
     viewModel: ActiveRunViewModel = koinViewModel()
 ) {
     ActiveRunScreen(
         state = viewModel.state,
+        onServiceToggle = onServiceToggle,
         onAction = viewModel::onAction
     )
 }
@@ -55,6 +58,7 @@ fun ActiveRunScreenRoot(
 @Composable
 fun ActiveRunScreen(
     state: ActiveRunState,
+    onServiceToggle: (isServiceRunning: Boolean) -> Unit,
     onAction: (ActiveRunAction) -> Unit,
     modifier: Modifier = Modifier
 ) {
@@ -106,6 +110,18 @@ fun ActiveRunScreen(
 
         if (!showLocationRationale && !showNotificationRationale) {
             permissionLauncher.requestRuniquePermissions(context)
+        }
+    }
+
+    LaunchedEffect(key1 = state.isRunFinished) {
+        if (state.isRunFinished) {
+            onServiceToggle(false)
+        }
+    }
+
+    LaunchedEffect(key1 = state.shouldTrack) {
+        if (context.hasLocationPermission() && state.shouldTrack && !ActiveRunService.isServiceActive) {
+            onServiceToggle(true)
         }
     }
 
@@ -261,6 +277,7 @@ private fun ActiveRunScreenPreview() {
     RuniqueTheme {
         ActiveRunScreen(
             state = ActiveRunState(),
+            onServiceToggle = {},
             onAction = {}
         )
     }
