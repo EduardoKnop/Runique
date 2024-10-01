1aab267932128b8d02bcee750f92a345b7537aaf
Eduardo Knop <eduk.coelho@hotmail.com>
2024-09-30 23:04:18 -0300

    Creating the Analytics Dynamic Feature Module

--- build-logic/convention/src/main/java/com/example/convention/BuildTypes.kt
+++ build-logic/convention/src/main/java/com/example/convention/BuildTypes.kt
@@ -3,6 +3,7 @@ package com.example.convention
 import com.android.build.api.dsl.ApplicationExtension
 import com.android.build.api.dsl.BuildType
 import com.android.build.api.dsl.CommonExtension
+import com.android.build.api.dsl.DynamicFeatureExtension
 import com.android.build.api.dsl.LibraryExtension
 import com.android.build.gradle.internal.cxx.configure.gradleLocalProperties
 import org.gradle.api.Project
@@ -45,6 +46,19 @@ internal fun Project.configureBuildTypes(
                     }
                 }
             }
+
+            ExtensionType.DYNAMIC_FEATURE -> {
+                extensions.configure<DynamicFeatureExtension> {
+                    buildTypes {
+                        debug {
+                            configureDebugBuildType(apiKey)
+                        }
+                        release {
+                            configureReleaseBuildType(commonExtension, apiKey)
+                        }
+                    }
+                }
+            }
         }
     }
 }
@@ -61,7 +75,7 @@ private fun BuildType.configureReleaseBuildType(
     buildConfigField("String", "API_KEY", "\"$apiKey\"")
     buildConfigField("String", "BASE_URL", "\"https://runique.pl-coding.com:8080\"")
 
-    isMinifyEnabled = true
+    /*isMinifyEnabled = true*/
     proguardFiles(
         commonExtension.getDefaultProguardFile("proguard-android-optimize.txt"),
         "proguard-rules.pro"
